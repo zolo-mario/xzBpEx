2f386953763c557dc814d0cf650621b34d0ad155
zoloypzuo <57245542+zolo-mario@users.noreply.github.com>
2026-03-28 23:52:34 +0800

    ew

--- BpGeneratorUltimate/Private/BpGeneratorUltimateCommands.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright 2026, BlueprintsLab, All rights reserved
-#include "BpGeneratorUltimateCommands.h"
-#define LOCTEXT_NAMESPACE "FBpGeneratorUltimateModule"
-void FBpGeneratorUltimateCommands::RegisterCommands()
-{
-	UI_COMMAND(PluginAction, "BpGeneratorUltimate", "Execute BpGeneratorUltimate action", EUserInterfaceActionType::Button, FInputChord());
-}
-#undef LOCTEXT_NAMESPACE
--- BpGeneratorUltimate/Public/MaterialNodeDescriber.h
+++
@@ -1,8 +0,0 @@
-// Copyright 2026, BlueprintsLab, All rights reserved
-#pragma once
-#include "CoreMinimal.h"
-class FMaterialNodeDescriber
-{
-public:
-    FString Describe(const TSet<UObject*>& SelectedNodes);
-};
